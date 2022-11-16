f93b4394b7a12e4861ce3a2d27427e31da21a1db
Carolina mora <carolinamorahernandez@hotmail.com>
2022-11-16 20:05:39 -0300

    Initial Commit

---
+++ assets/js/todolist.js
@@ -0,0 +1,68 @@
+const inputTarea = document.getElementById("tarea");
+const btnAgregar = document.getElementById("agregarTarea");
+const listaDeTareas = document.querySelector("tbody");
+const botonBorrar = document.getElementById("borrar");
+let tareasTotales = document.querySelector("#total");
+let tareasRealizadas = document.querySelector("#realizadas");
+let id = 0;
+
+
+function renderTareas(array = tareas){
+    let html = "";
+    for (let tarea of array){
+        html += `<tr>
+                    <td>${tarea.id}</td>
+                    <td colspan="2">${tarea.nombre}</td>
+                    <td><input class="form-check-input mt-0" type="checkbox" onclick="cambiarValorCheckbox(${tarea.id})" ${tarea.estado ? "checked" : ""} ></td>
+                    <td><button onclick="borrarTarea(${tarea.id})">❌</button></td>
+                </tr>`;
+    }
+
+    
+    const realizadas = array.filter((check) => check.estado === true);
+    const contador = realizadas.length;
+    tareasRealizadas.innerHTML = contador;
+    tareasTotales.innerHTML = array.length
+    listaDeTareas.innerHTML = html
+}
+
+const tareas = [];
+
+btnAgregar.addEventListener('click', () => {
+    const nombreTarea = inputTarea.value;
+    id++
+
+    if (nombreTarea != ""){
+    tareas.push({id: id, nombre: nombreTarea, estado: false});
+    inputTarea.value = "";
+
+    renderTareas()
+    } else {
+        alert("Debes ingresar una tarea")
+    }
+})
+
+
+function borrarTarea(id){
+    const index = tareas.findIndex((ele)=> ele.id == id)
+    tareas.splice(index, 1)
+
+    renderTareas()
+}
+
+function cambiarValorCheckbox(id){
+    tareas.map((tarea) => {
+        if (tarea.id == id) tarea.estado = !tarea.estado;
+    });
+    
+    renderTareas()
+}
+
+
+
+
+
+
+
+
+
